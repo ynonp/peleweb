4e586e21bd72e14bebf26ea43fe62db876d8aaf8
ynonperek <ynonperek@gmail.com>
2015-09-17 10:53:05 +0300

    add $scope.$apply example

--- demos/13_angular/demo.js
+++ demos/13_angular/demo.js
@@ -10,16 +10,33 @@ var myapp = angular.module('app', []);
 myapp.controller('mainCtrl', MainController);
 
 
-function MainController() {
+MainController.$inject = ['$scope'];
+function MainController($scope) {
   var vm = this;
 
   vm.clicks  = 0;
   vm.name    = "Ynon";
   vm.email   = "ynon@ynonperek.com";
   vm.website = 'https://www.tocode.co.il';
+  vm.bravo   = false;
+  vm.seconds = 0;
+
+  setInterval(function() {
+    vm.seconds++;
+    // Hey Angular! something's changed
+    // please re-calculate ALL THE THINGS
+    $scope.$apply();
+  }, 1000);
 
   vm.clicked = function() {
     vm.clicks++;
+    vm.bravo = (vm.clicks >= 10); 
   };
+
+  vm.cps = function() {
+    return vm.seconds > 0 ? vm.clicks / vm.seconds : 0;
+  };
+
+
 }
 
